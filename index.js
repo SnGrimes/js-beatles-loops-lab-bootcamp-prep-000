2efa3e658e4546368abe22da10eac62e9d916fd6
Shantia Grimes-Farmer <shantiagrimes@gmail.com>
2018-06-08 15:15:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function theBeatlesPlay() {
-  
+function theBeatlesPlay(musicianArr, instrumentsArr) {
+  var array = [];
+  for(let i = 0; i < musicianArr.length; i++) {
+    array[i].push(`${musicianArr[i]} plays ${instrumentsArr[i]}`);
+  }
+  return array;
 }
